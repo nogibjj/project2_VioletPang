db586139a0fda3a0fd21bce3d861cc6990671bf2
Ultraviolet0909 <65413798+Ultraviolet0909@users.noreply.github.com>
2022-09-27 19:34:20 +0000

    Update noew methods

--- bash-cli-tool/main.py
+++ bash-cli-tool/main.py
@@ -1,41 +1,39 @@
 import pandas as pd
-import psycopg2
+from pandasql import sqldf
 import typer
 from psycopg2.extras import NamedTupleCursor
 from tabulate import tabulate
 
 app = typer.Typer()
 
-conn = psycopg2.connect(
-    dbname="nfp_boilerplate_dev",
-    user="nfp_boilerplate_user"
-)
-
-cur = conn.cursor(cursor_factory=NamedTupleCursor)
+@app.command()
+def loadData(): 
+    """Load dataset"""
+    df = pd.read_csv('menu.csv')
+    table = df.iloc[[0,1],:]
+    print("Successfully load dataset")
+    print(table)
 
 @app.command()
-def example1():
-    """fetch notes"""
-    cur.execute("SELECT * FROM notes")
-    records = cur.fetchall()
-    print(tabulate(records, headers="keys", tablefmt="psql"))
+def fetchlist():
+    """fetch items list"""
+    df = pd.read_csv('menu.csv')
+    print(df)
+
 
 @app.command()
-def example2():
-    """fetch notes and load into pandas"""
-    cur.execute("SELECT * FROM notes")
-    records = cur.fetchall()
-    df = pd.DataFrame(records)
-    print(tabulate(df, headers="keys", tablefmt="psql"))
+def sortbycalories():
+    """return sorted list by calories"""
+    df = pd.read_csv('menu.csv')
+    sorted = df.sort_values(by=['Calories'],ascending = False)
+    print(sorted[['Item','Calories']])
 
 @app.command()
-def example3(id: int):
-    """fetch note by id"""
-    cur.execute("SELECT * FROM notes WHERE id=%s", (id,))
-    records = cur.fetchall()
-    df = pd.DataFrame(records)
-    df = df.transpose()
-    print(tabulate(df, headers="keys", tablefmt="psql"))
+def seemax():
+    """See max value of each dataset"""
+    df = pd.read_csv('menu.csv')
+    max_values = df[['Calories','Total Fat','Carbohydrates','Dietary Fiber','Sugars','Protein','Vitamin A (% Daily Value)','Vitamin C (% Daily Value)','Calcium (% Daily Value)','Iron (% Daily Value)']].max()
+    print(max_values)
 
 if __name__ == "__main__":
     app()
